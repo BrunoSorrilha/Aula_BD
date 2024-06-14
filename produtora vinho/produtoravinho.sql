4859453f054081685766d20061d1474e89705c97
Bruno_Sorrilha <124791792+BrunoSorrilha@users.noreply.github.com>
2024-06-14 12:46:19 -0300

    Add files via upload

--- produtora vinho/produtoravinho.sql
+++ produtora vinho/produtoravinho.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07/06/2024 às 17:44
+-- Tempo de geração: 14/06/2024 às 17:45
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -34,6 +34,18 @@ CREATE TABLE `casta` (
   `casta` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `casta`
+--
+
+INSERT INTO `casta` (`codCasta`, `casta`) VALUES
+(1, 'Touriga Nacional'),
+(2, 'Tinta Roriz'),
+(3, 'Tinta Barroca'),
+(4, 'Alvarinho'),
+(5, 'Cabernet Sauvignon'),
+(6, 'Malbec');
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +56,18 @@ CREATE TABLE `casta_vinho` (
   `codVinho` int(11) DEFAULT NULL,
   `codCasta` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `casta_vinho`
+--
+
+INSERT INTO `casta_vinho` (`codVinho`, `codCasta`) VALUES
+(1, 6),
+(2, 5),
+(3, 4),
+(4, 3),
+(5, 2),
+(6, 1);
 
 -- --------------------------------------------------------
 
@@ -59,6 +83,20 @@ CREATE TABLE `produtor` (
   `email` varchar(50) DEFAULT NULL,
   `codregiao` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produtor`
+--
+
+INSERT INTO `produtor` (`produtorid`, `nomeProdutor`, `moradaProdutor`, `telefone`, `email`, `codregiao`) VALUES
+(1, 'Qt. Vallado', 'Régua', '254323147', 'vallado@gmail.telepac.pt', 1),
+(2, 'Finagra', 'Reguengos', '266509270', 'esporao@esporao.com', 2),
+(3, 'Brayan', 'quarta', '254323865', 'brayan@gmail.telepac.pt', 3),
+(4, 'maya', 'Piraporinha', '252343147', 'maya@gmail.com.br', 4),
+(5, 'prayan', 'jundiai', '254603147', 'prayan@gmail.telepac.pt', 5),
+(6, 'Alameda', 'caetano', '037423147', 'alameda@gmail.com.br', 6),
+(7, 'Daniel', 'kelvin', '282749147', 'vdaniel@gmail.com.br', 7),
+(8, 'Juan', 'grond', '254999767', 'juan@gmail.com.br', 8);
 
 -- --------------------------------------------------------
 
@@ -100,6 +138,18 @@ CREATE TABLE `vinho` (
   `preco` decimal(7,2) DEFAULT NULL,
   `codProdutor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `vinho`
+--
+
+INSERT INTO `vinho` (`vinhoId`, `nomeVinho`, `ano_vinho`, `cor`, `grau`, `preco`, `codProdutor`) VALUES
+(1, 'Esporão Reserva', 2004, 'Tinto', 14.50, 18.50, 1),
+(2, 'Quinta do Vallado', 1245, 'Tinto', 14.00, 6.50, 2),
+(3, 'Muros Antigos', 2006, 'Branco', 13.00, 7.50, 3),
+(4, 'Albariño', 2012, 'Branco', 16.00, 100.50, 4),
+(5, 'Carménère', 2024, 'Tinto', 7.50, 2.00, 5),
+(6, 'Chardonnay', 1984, 'Branco', 12.50, 2.50, 6);
 
 --
 -- Índices para tabelas despejadas
@@ -147,13 +197,13 @@ ALTER TABLE `vinho`
 -- AUTO_INCREMENT de tabela `casta`
 --
 ALTER TABLE `casta`
-  MODIFY `codCasta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `codCasta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `produtor`
 --
 ALTER TABLE `produtor`
-  MODIFY `produtorid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `produtorid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `regiao`
@@ -165,7 +215,7 @@ ALTER TABLE `regiao`
 -- AUTO_INCREMENT de tabela `vinho`
 --
 ALTER TABLE `vinho`
-  MODIFY `vinhoId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `vinhoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restrições para tabelas despejadas
