980dbb9e9cf28eac20dc3c83d6f4b60992292b6b
Bruno_Sorrilha <124791792+BrunoSorrilha@users.noreply.github.com>
2024-09-16 08:04:30 -0300

    Add files via upload

---
+++ innerjoinn/estacionamento.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 16-Set-2024 às 13:03
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `estacionamento`
+--
+CREATE DATABASE IF NOT EXISTS `estacionamento` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `estacionamento`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `andar`
+--
+
+CREATE TABLE `andar` (
+  `codLugar` int(11) NOT NULL,
+  `capacidade` int(11) DEFAULT NULL,
+  `andar` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cpf` varchar(20) NOT NULL,
+  `dtnasc` datetime DEFAULT NULL,
+  `nome` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estaciona`
+--
+
+CREATE TABLE `estaciona` (
+  `cod` int(11) NOT NULL,
+  `horSaida` datetime DEFAULT NULL,
+  `dtEntrada` datetime DEFAULT NULL,
+  `horEntrada` datetime DEFAULT NULL,
+  `dtSaida` datetime DEFAULT NULL,
+  `placa` int(11) DEFAULT NULL,
+  `codLugar` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `modelo`
+--
+
+CREATE TABLE `modelo` (
+  `codModelo` int(11) NOT NULL,
+  `modelo` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `veiculo`
+--
+
+CREATE TABLE `veiculo` (
+  `placa` int(11) NOT NULL,
+  `cor` varchar(20) DEFAULT NULL,
+  `cpf` varchar(20) DEFAULT NULL,
+  `codModelo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `andar`
+--
+ALTER TABLE `andar`
+  ADD PRIMARY KEY (`codLugar`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Índices para tabela `estaciona`
+--
+ALTER TABLE `estaciona`
+  ADD PRIMARY KEY (`cod`),
+  ADD KEY `placa` (`placa`),
+  ADD KEY `codLugar` (`codLugar`);
+
+--
+-- Índices para tabela `modelo`
+--
+ALTER TABLE `modelo`
+  ADD PRIMARY KEY (`codModelo`);
+
+--
+-- Índices para tabela `veiculo`
+--
+ALTER TABLE `veiculo`
+  ADD PRIMARY KEY (`placa`),
+  ADD KEY `cpf` (`cpf`),
+  ADD KEY `codModelo` (`codModelo`);
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `estaciona`
+--
+ALTER TABLE `estaciona`
+  ADD CONSTRAINT `estaciona_ibfk_1` FOREIGN KEY (`placa`) REFERENCES `veiculo` (`placa`),
+  ADD CONSTRAINT `estaciona_ibfk_2` FOREIGN KEY (`codLugar`) REFERENCES `andar` (`codLugar`);
+
+--
+-- Limitadores para a tabela `veiculo`
+--
+ALTER TABLE `veiculo`
+  ADD CONSTRAINT `veiculo_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `cliente` (`cpf`),
+  ADD CONSTRAINT `veiculo_ibfk_2` FOREIGN KEY (`codModelo`) REFERENCES `modelo` (`codModelo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
